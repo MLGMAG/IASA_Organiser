75d3e5616398e3462d67e00b9bacf281a40415a9
Mahomed Akhmedov <akhmedovmagomed2001@gmail.com>
2020-09-26 14:23:17 +0300

    Added simple logging

--- src/main/java/ua/kpi/iasa/IASA_Organiser/service/data/ArrayDataManager.java
+++ src/main/java/ua/kpi/iasa/IASA_Organiser/service/data/ArrayDataManager.java
@@ -1,5 +1,7 @@
 package ua.kpi.iasa.IASA_Organiser.service.data;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ua.kpi.iasa.IASA_Organiser.model.Event;
 
 import java.util.UUID;
@@ -7,10 +9,11 @@ import java.util.UUID;
 public class ArrayDataManager implements DataManager {
 
     private EventStore data = new EventStore();
+    private static final Logger logger = LoggerFactory.getLogger(ArrayDataManager.class);
 
     @Override
     public boolean save(Event event) {
-        System.out.println(event);
+        logger.debug("Saving event object: {}", event);
         event.setId(UUID.randomUUID());
         if (data.getSize() == data.getMaxSize()) {
             return false;
