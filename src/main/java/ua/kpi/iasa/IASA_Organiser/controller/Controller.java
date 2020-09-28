b86173f22afb18fe13b7124518850a8dad7f00be
Mahomed Akhmedov <akhmedovmagomed2001@gmail.com>
2020-09-28 22:22:36 +0300

    Removed logger

--- src/main/java/ua/kpi/iasa/IASA_Organiser/controller/Controller.java
+++ src/main/java/ua/kpi/iasa/IASA_Organiser/controller/Controller.java
@@ -1,7 +1,5 @@
 package ua.kpi.iasa.IASA_Organiser.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import ua.kpi.iasa.IASA_Organiser.model.Event;
 import ua.kpi.iasa.IASA_Organiser.service.EventService;
 import ua.kpi.iasa.IASA_Organiser.view.ConsoleManager;
@@ -11,14 +9,12 @@ public class Controller {
 
     private EventService eventService = new EventService();
     private ConsoleManager consoleManager = new ConsoleManager();
-    private static final Logger logger = LoggerFactory.getLogger(Controller.class);
 
     public boolean createNewEvent(Event event) {
         return eventService.createEvent(event);
     }
 
     public Controller() {
-        logger.info("We are starting!!");
         consoleManager.setController(this);
         consoleManager.startUp();
     }
--- src/main/java/ua/kpi/iasa/IASA_Organiser/service/data/ArrayDataManager.java
+++ src/main/java/ua/kpi/iasa/IASA_Organiser/service/data/ArrayDataManager.java
@@ -1,7 +1,5 @@
 package ua.kpi.iasa.IASA_Organiser.service.data;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import ua.kpi.iasa.IASA_Organiser.model.Event;
 
 import java.util.UUID;
@@ -9,11 +7,9 @@ import java.util.UUID;
 public class ArrayDataManager implements DataManager {
 
     private EventStore data = new EventStore();
-    private static final Logger logger = LoggerFactory.getLogger(ArrayDataManager.class);
 
     @Override
     public boolean save(Event event) {
-        logger.debug("Saving event object: {}", event);
         event.setId(UUID.randomUUID());
         if (data.getSize() == data.getMaxSize()) {
             return false;
