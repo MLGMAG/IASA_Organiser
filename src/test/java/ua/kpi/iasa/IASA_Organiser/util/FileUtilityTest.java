ce3621af2a1d18e1bf2667a5d5e8ea9baadad650
Mahomed Akhmedov <mahomed_akhmedov@epam.com>
2020-11-19 21:31:56 +0200

    [FileUtility] Add unit tests

---
+++ src/test/java/ua/kpi/iasa/IASA_Organiser/util/FileUtilityTest.java
@@ -0,0 +1,101 @@
+package ua.kpi.iasa.IASA_Organiser.util;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.junit.MockitoJUnitRunner;
+import ua.kpi.iasa.IASA_Organiser.model.Event;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.Collections;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class FileUtilityTest {
+
+    @Mock
+    private List<Event> eventList;
+
+    @Mock
+    private File file;
+
+    @Mock
+    private Event event;
+
+    @Mock
+    private ObjectOutputStream objectOutputStream;
+
+    @Mock
+    private MockedStatic<FileUtility> fileUtilityMocked;
+
+//    @Test
+//    public void shouldParseData() {
+//        fileUtilityMocked.when(() -> FileUtility.saveListToFile(file, eventList)).thenCallRealMethod();
+//
+//        FileUtility.parseData(file, eventList);
+//    }
+
+    @Test
+    public void shouldSaveListToFile() {
+        List<Event> testList = asList(event, event, event);
+        fileUtilityMocked.when(() -> FileUtility.getObjectOutputStream(file)).thenReturn(objectOutputStream);
+        fileUtilityMocked.when(() -> FileUtility.saveListToFile(file, testList)).thenCallRealMethod();
+
+        List<Event> actual = FileUtility.saveListToFile(file, testList);
+
+        fileUtilityMocked.verify(times(3), () -> FileUtility.writeObjectToFile(objectOutputStream, event));
+        assertEquals(testList, actual);
+    }
+
+    @Test
+    public void shouldNotSaveListToFile() {
+        List<Event> testList = asList(event, event, event);
+        fileUtilityMocked.when(() -> FileUtility.getObjectOutputStream(file)).thenReturn(objectOutputStream);
+        fileUtilityMocked.when(() -> FileUtility.writeObjectToFile(objectOutputStream, event)).thenThrow(IOException.class);
+        fileUtilityMocked.when(() -> FileUtility.saveListToFile(file, testList)).thenCallRealMethod();
+
+        List<Event> actual = FileUtility.saveListToFile(file, testList);
+
+        assertEquals(Collections.emptyList(), actual);
+    }
+
+    @Test
+    public void shouldInitFile() throws IOException {
+        when(file.createNewFile()).thenReturn(true);
+        fileUtilityMocked.when(() -> FileUtility.initFile(file)).thenCallRealMethod();
+
+        try {
+            FileUtility.initFile(file);
+        } catch (IOException e) {
+            fail();
+        }
+    }
+
+    @Test(expected = IOException.class)
+    public void shouldNotInitFileOnException() throws IOException {
+        when(file.createNewFile()).thenReturn(false);
+        fileUtilityMocked.when(() -> FileUtility.initFile(file)).thenCallRealMethod();
+
+        FileUtility.initFile(file);
+    }
+
+    @Test
+    public void shouldGetNewFile() {
+        String test = "testDir";
+        fileUtilityMocked.when(() -> FileUtility.getNewFile(test)).thenCallRealMethod();
+
+        File newFile = FileUtility.getNewFile(test);
+
+        assertEquals(test, newFile.getPath());
+
+    }
+}
