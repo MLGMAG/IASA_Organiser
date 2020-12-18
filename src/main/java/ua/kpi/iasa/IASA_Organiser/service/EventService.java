afe15408b6390e449b18926268230c6ceb84a01b
Mahomed Akhmedov <mahomed_akhmedov@epam.com>
2020-12-18 10:33:56 +0200

    [RemoveEventError] Fix 500 on removing event

--- src/main/java/ua/kpi/iasa/IASA_Organiser/service/EventService.java
+++ src/main/java/ua/kpi/iasa/IASA_Organiser/service/EventService.java
@@ -2,16 +2,12 @@ package ua.kpi.iasa.IASA_Organiser.service;
 
 import org.springframework.stereotype.Service;
 import ua.kpi.iasa.IASA_Organiser.model.Event;
-import ua.kpi.iasa.IASA_Organiser.model.Human;
-import ua.kpi.iasa.IASA_Organiser.model.Place;
 import ua.kpi.iasa.IASA_Organiser.repository.EventRepository;
 import ua.kpi.iasa.IASA_Organiser.repository.HumanRepository;
 
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
@@ -58,32 +54,20 @@ public class EventService {
     }
 
     public Event getEventById(UUID id) {
-        Optional<Event> optEvent = eventRepository.findById(id);
-        if (optEvent.isEmpty()) {
-            return null;
-        }
-        return optEvent.get();
+        return eventRepository.findById(id).orElse(null);
     }
 
     public void deleteById(UUID id) {
-        Optional<Event> optEvent = eventRepository.findById(id);
-        if (optEvent.isEmpty()) {
-            return;
-        }
-        Event event = optEvent.get();
-        if (event.getInvited().isEmpty()) {
-            eventRepository.deleteById(id);
-            return;
-        }
-        List<Human> allHumans = humanRepository.findAllById(event.getInvited().stream().map(Human::getId).collect(Collectors.toSet()));
-        allHumans.forEach(human -> {
-            Set<Event> events = human.getEvents().stream().filter(event1 -> !event1.getId().equals(event.getId())).collect(Collectors.toSet());
-            human.setEvents(events);
-            humanRepository.save(human);
-        });
-        event.setInvited(Collections.emptySet());
-        eventRepository.save(event);
-        eventRepository.delete(event);
+        eventRepository.findById(id)
+                .ifPresent(event -> event.getInvited()
+                        .stream()
+                        .map(human -> humanRepository.findHumanAndEvents(human.getId()))
+                        .filter(Optional::isPresent)
+                        .map(Optional::get).forEach(human -> {
+                            human.getEvents().remove(event);
+                            humanRepository.save(human);
+                        }));
+        eventRepository.deleteById(id);
     }
 
     List<Event> sortEventsByPriority(List<Event> events) {
