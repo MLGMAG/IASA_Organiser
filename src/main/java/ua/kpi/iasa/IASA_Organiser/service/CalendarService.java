b57a64a5989cbfc49662223c6aeded2edb79ed5c
VasiliyPetrov1 <liashko2002@gmail.com>
2020-12-22 23:00:00 +0200

    [CalendarService] Add documentation

--- src/main/java/ua/kpi/iasa/IASA_Organiser/service/CalendarService.java
+++ src/main/java/ua/kpi/iasa/IASA_Organiser/service/CalendarService.java
@@ -16,6 +16,20 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
+
+/**
+ * This class is used for making basic operations on events {@link Event}
+ * when we need to consider their state, defined by the state of their fields
+ * like {@code date}, {@code time} and {@code priority} {@link Priority} etc.
+ * For example, we can find special events, sort Events by given feature
+ * and get the sequence of all events with special characteristics given
+ * by the user via methods declared in this class.
+ * @author Oleksii Kosiuk
+ * @author Andrij Makrushin
+ * @author Mahomed Akhmedov
+ * @see DefaultFileDataManager
+ * @see ua.kpi.iasa.IASA_Organiser.controller.Controller
+ */
 public class CalendarService {
     private static CalendarService instance;
     private final GenericDataManager dataManager = DefaultFileDataManager.getInstance();
@@ -27,12 +41,26 @@ public class CalendarService {
     /**
      * @deprecated uses deprecated class
      */
-    @Deprecated
+    @Deprecated(since = "MATA 1.0.0", forRemoval = false)
     public Calendar getCalendar() {
         logger.debug("Method was called...");
         throw new UnsupportedOperationException("You refer to unsupported method");
     }
 
+    /**
+     * This method finds all {@link Event} events in data storage with the value of their {@code date} field
+     * equal to value searchDate given as a single parameter and returns them.
+     * <p>
+     * Receives instance of LocalDate {@link LocalDate}.
+     * Obtains the list which contains all events stored in data storage {@link DefaultFileDataManager#getFile()}
+     * by calling {@link DefaultFileDataManager#getAllEventsList()} method and makes the copy of that list.
+     * Then returns list of all events from copied list with value of their date {@code date} field
+     * equal to received parameter.
+     *
+     * @param searchDate instance of LocalDate, events with date field value equal to which we want to find
+     * @return list of events with {@code date} field value equal to given parameter
+     * @see DefaultFileDataManager
+     */
     public List<Event> findEventsByDate(LocalDate searchDate) {
         logger.debug("Method was called with {}", searchDate);
         final List<Event> events = getDataManager().getAllEventsList();
@@ -41,6 +69,21 @@ public class CalendarService {
                 .collect(Collectors.toList());
     }
 
+    /**
+     * This method finds all {@link Event} events in data storage, which have tag{@link Tag} instance equal to single
+     * given searchTag parameter in their {@code tags} field which represents the list of tags. And then returns all
+     * found events.
+     * <p>
+     * Receives instance of Tag {@link Tag}.
+     * Obtains the list which contains all events stored in data storage {@link DefaultFileDataManager#getFile()}
+     * by calling {@link DefaultFileDataManager#getAllEventsList()} method and makes the copy of that list.
+     * Then returns list of all events from copied list which have Tag instance equal to received parameter
+     * in their tag list {@code tags} field.
+     *
+     * @param searchTag
+     * @return list of events with given tag parameter in their tag list {@code tags} field
+     * @see DefaultFileDataManager
+     */
     public List<Event> findEventsByTag(Tag searchTag) {
         logger.debug("Method was called with {}", searchTag);
         final List<Event> events = getDataManager().getAllEventsList();
@@ -49,6 +92,20 @@ public class CalendarService {
                 .collect(Collectors.toList());
     }
 
+    /**
+     * This method finds all {@link Event} events in data storage with the value of their {@code priority} field
+     * equal to value searchPriority given as a single parameter and returns them.
+     * <p>
+     * Receives instance of Priority {@link Priority}.
+     * Obtains the list which contains all events stored in data storage {@link DefaultFileDataManager#getFile()}
+     * by calling {@link DefaultFileDataManager#getAllEventsList()} method and makes the copy of that list.
+     * Then returns list of all events from copied list with value of their {@code priority} field
+     * equal to received parameter.
+     *
+     * @param searchPriority instance of Priority, events with priority field value equal to which we want to find
+     * @return list of events with {@code priority} field value equal to given parameter
+     * @see DefaultFileDataManager
+     */
     public List<Event> findEventsByPriority(Priority searchPriority) {
         logger.debug("Method was called with {}", searchPriority);
         final List<Event> events = getDataManager().getAllEventsList();
@@ -57,7 +114,17 @@ public class CalendarService {
                 .collect(Collectors.toList());
     }
 
-
+    /**
+     * This method sorts all {@link Event} events in data storage by their {@code priority} field and returns the list
+     * of sorted events.
+     * <p>
+     * Obtains the list which contains all events stored in data storage {@link DefaultFileDataManager#getFile()}
+     * by calling {@link DefaultFileDataManager#getAllEventsList()} method and makes the copy of that list.
+     * Then sorts copied list by {@code priority} and returns it.
+     *
+     * @return list of events sorted by their {@code priority} field
+     * @see DefaultFileDataManager
+     */
     public List<Event> sortEventsByPriority() {
         logger.debug("Method was called...");
         final List<Event> allEventsList = getDataManager().getAllEventsList();
@@ -65,6 +132,16 @@ public class CalendarService {
         return allEventsList;
     }
 
+    /**
+     * This method sorts all {@link Event} events in list given as a parameter by their {@code date} field and returns
+     * the list of sorted events.
+     * <p>
+     * Receives list of {@link Event} events which should be sorted.
+     * Then sorts copied list by {@code date} field and returns it.
+     *
+     * @param sortedList list of events which should be sorted by {@code date} field
+     * @return given as an argument list of events sorted by {@code date}
+     */
     public List<Event> sortEventsByDate(List<Event> sortedList) {
         sortedList.sort((ev1, ev2) -> {
             int yearDif = ev1.getDate().getYear() - ev2.getDate().getYear();
@@ -79,6 +156,21 @@ public class CalendarService {
         return sortedList;
     }
 
+    /**
+     * This method returns all {@link Event} events in data storage which should happen before {@link LocalDate} date
+     * given as a parameter.
+     * <p>
+     * Receives instance of {@link LocalDate}.
+     * Obtains the list which contains all events stored in data storage {@link DefaultFileDataManager#getFile()}
+     * by calling {@link DefaultFileDataManager#getAllEventsList()} method and makes the copy of that list.
+     * Creates new empty list of events and fills it with events which have {@code date} field, which represents the
+     * date before date, which is given as a parameter.
+     *
+     * @param date date, events before which we need to return
+     * @return list of events which have {@code date} field, which represents the
+     * date before date, which is given as a parameter.
+     * @see DefaultFileDataManager
+     */
     public List<Event> getEventsBeforeDate(LocalDate date) {
         int year = date.getYear();
         int day = date.getDayOfYear();
